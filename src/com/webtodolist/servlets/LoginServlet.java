599c6587cc5f42bea40e58f2cab86d8dbc434385
julien <julien.def@outlook.fr>
2020-11-06 09:59:43 +0100

    Ajout fichier projets

--- src/com/webtodolist/servlets/LoginServlet.java
+++ src/com/webtodolist/servlets/LoginServlet.java
@@ -16,7 +16,7 @@ import com.webtodolist.beans.UserBean;
 import com.webtodolist.database.UserDBUtil;
 
 @WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet  {
 	private static final long serialVersionUID = 1L;
       
 	private UserDBUtil userDBUtil;
